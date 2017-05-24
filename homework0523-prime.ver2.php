1652f5726c0f2876ca143fc054c3afbdd33c3b90
falock1007 <fantasticfalco@gmail.com>
2017-05-24 20:27:18 +0800

    Add files via upload

---
+++ homework0523-prime.ver2.php
@@ -0,0 +1,30 @@
+<table border="1" width="50%" align="center">
+
+    <?php $col = 10; $max=100000;
+for($x=1;$x<=$max;$x++){
+    $z=0;
+    for ($y = 1; $y <= sqrt($x); $y++) {
+        if($x%$y==0){
+            $z++;
+            if($z>1){
+                break;
+            }
+        }
+    }
+    if($x==1 || $z!=1){
+        $n = "<td>" . $x . "</td>";
+    } else {
+        $n = "<td bgcolor='orange'>" . $x . "</td>";
+
+    }
+    if($x%$col==1){
+        echo "<tr>" . $n;
+    } elseif($x%$col==0){
+        echo $n . "</tr>";
+    } else{
+        echo $n;
+}}
+?>
+
+</table>
+
