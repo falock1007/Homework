dd60f510b68bcbe294691d62e5e40c7fdd8dc2fc
falock1007 <fantasticfalco@gmail.com>
2017-06-09 18:17:55 +0800

    Update homework0609-swap.swift

--- homework0609-swap.swift
+++ homework0609-swap.swift
@@ -1,10 +1,12 @@
 func swapArray( _ array:inout Array<Int>, _ x:Int, _ y:Int) {
     if array.isEmpty {
         print("Wrong01 : 陣列請勿為空")
-    }else if array.count < x || array.count < y {
-        x == y ? print("Wrong03 : x與y超出範圍") : (x > y ? print("Wrong03 : x超出範圍"):print("Wrong03 : y超出範圍"))
-    }else if array.count < 2 {
-        print("Wrong02 : 陣列太短")
+    }else if x == y { print("Wrong02 : x和y請勿一樣")
+    }else if array.count-1 < x || array.count-1 < y {
+        array.count-1 < x && array.count-1 < y ? print("Wrong03: x與y都超過範圍") :
+        (x > y ? print("Wrong03 : x超出範圍"):print("Wrong03 : y超出範圍"))
+    }else if x < 0 || y < 0{
+        x < 0 && y < 0 ? print("Wrong04 : x值與y值請勿為負值") : (x < 0 ? print("Wrong04 : x值請勿為負值") : print("Wrong04 : y值請勿為負值"))
     }else{
         swap(&array[x], &array[y])
 //        let temp = array[x]
@@ -15,5 +17,5 @@ func swapArray( _ array:inout Array<Int>, _ x:Int, _ y:Int) {
 }
 
 var a6:[Int] = [1,2,3,4,5]
-var a7:[Int] = []
-swapArray(&a6,2,4)
+var a7:[Int] = [1]
+swapArray(&a7,1,2)
